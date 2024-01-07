72938d34cc44741df13f2b631a797d0f3f1e4a93
ijadnazar.mahdi <ijadnazar.mahdi@gmail.com>
2024-01-06 21:10:39 -0500

    Initial Commit

---
+++ UIList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIList
+//
+//  Created by Mehdi Ijadnazar on 10/16/1402 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
